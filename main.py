74438b5bb53349593022692d7ffa2ae323c3bafe
Blinova_Tatiana_417 <nash101@mail.ru>
2024-05-21 09:47:36 +0300

    Commit three

--- main.py
+++ main.py
@@ -2,5 +2,18 @@ print("My name is Tanya")
 
 num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
-result = num1 + num2
-print("Result", result)
+
+operation = input("Выберите операцию (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 * num2
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    result = num1 / num2
+else:
+    print("Неверная операция!")
+
+print("Результат:", result)
